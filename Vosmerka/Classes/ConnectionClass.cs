e2521923defff6967e7fae9603d156c963aeb85a
Nasty <120274416+IBarashek@users.noreply.github.com>
2025-04-07 20:57:24 +0300

    Добавьте файлы проекта.

---
+++ Vosmerka/Classes/ConnectionClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vosmerka.DB;
+
+namespace Vosmerka.Classes
+{
+    class ConnectionClass
+    {
+        public DEntities entities = new DEntities();
+    }
+}
